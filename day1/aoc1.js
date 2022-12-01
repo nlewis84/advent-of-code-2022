5f814a4b816f89a150a3b043a59acf9296aece8a
nlewis84 <nathan@differential.com>
2022-12-01 07:20:47 -0600

    feat: day 1 complete

--- day1/aoc1.js
+++ day1/aoc1.js
@@ -2,5 +2,40 @@ const { aoc_input } = require('../config');
 const fs = require('fs');
 const lines = fs
 	.readFileSync(`${aoc_input}`, 'utf-8')
+  .split('\n')
 
-console.log(lines)
+// Part 1
+// Return the highest number in the array.
+// I thought we might need to remember the elf's index,
+// otherwise I would have sorted lines and return index 0.
+
+let elfTotals = [];
+let currentSum = 0;
+for (let i = 0; i < lines.length; i++) {
+  if (lines[i] === '') {
+    elfTotals.push(currentSum);
+    currentSum = 0;
+  } else {
+    currentSum += parseInt(lines[i]);
+  }
+}
+
+let highest = 0;
+let highestIndex = 0;
+for (let i = 0; i < elfTotals.length; i++) {
+  if (elfTotals[i] > highest) {
+    highest = elfTotals[i];
+    highestIndex = i;
+  }
+}
+
+console.log(highest, highestIndex);
+
+// Part Two
+// Find the top three elves and sum their totals
+// Since we don't need index, we can sort the array.
+
+// sort elfTotals and return the sum of the first three items
+elfTotals.sort((a, b) => b - a);
+
+console.log(elfTotals[0] + elfTotals[1] + elfTotals[2]);
